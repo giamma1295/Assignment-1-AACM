a339ef8cd5322d798b941d46fd832a5c0a594627
Gianmaria Scorza <scogiam95@gmail.com>
2019-11-28 14:24:47 +0100

    implemented get_edge, edges, added dictionary to store the graph

--- Graph.py
+++ Graph.py
@@ -13,11 +13,17 @@ class Graph:
         g = [] #will be a list of sets(nodo1 -> int, nodo2 -> int, peso float)
         #la edge list contine per ogni riga la tupla nodo1 nodo2 peso
         #ciclo per le varie righe del file
+        dictG = {} #dictionary {key -> (u,v) : value -> weight}
         for linea in graphFile.readlines():
             edge = linea.split()
             edge = (int(edge[0]),int(edge[1]),float(edge[2]))
             g.append(edge)
+            dictG.update({'('+str(edge[0])+','+str(edge[1])+')' : float(edge[2])})
+        pass
+        #print(dictG)
+        #print('richiesto : ' + str(dictG['(131,146)']))
         self.graph = g
+        self.dictGraph = dictG
         pass
 
 
@@ -48,14 +54,36 @@ class Graph:
             vertici.sort()
             pass
         if log:
-            print("Vertrex List -> \n " + str(vertici))
+            print("Vertrex List  -> \n " + str(vertici))
+            #print('\n'.join(map(str, vertici)))
             pass
         return vertici
 
     def edge_count(self, log=False):
         n = len(self.graph)
-
         if log:
             print('number of edge in given graph -> ' + str(n) )
             pass
         return n
+
+    def edges(self, log=False):
+        if log:
+            print('edges in given graph : ')
+            print('\n'.join(map(str, map(lambda x: {'nodo -> ':x[1], 'id -> ':x[0]}, enumerate(self.graph)))))
+            pass
+        return self.graph
+
+    def get_edge(self, u, v, log=False):
+        selected = None
+        for ver in self.graph:
+            if (ver[0] == u and ver[1] == v) or ((ver[1] == u and ver[0] == v)) :
+                selected = ver
+                pass
+        if log:
+            if selected == None:
+                print("requested vertrex not Found ")
+            else:
+                print("requested vertrex -> " + str(selected))
+            pass
+        return selected
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-%load_ext autoreload
-%autoreload 2
+#%load_ext autoreload
+#%autoreload 2
 from Graph import Graph
 
 
@@ -8,22 +8,29 @@ from Graph import Graph
 grafo = Graph('grafi/USAir97.net')
 
 #print del grafo
-grafo.printGrafo()
+#grafo.printGrafo()
 
 #vertex_count
 #con log
-grafo.vertex_count(True)
+#grafo.vertex_count(True)
 
 #senza log
-grafo.vertex_count()
+grafo.vertex_count(True)
 
 #vertices
 #con log
-grafo.vertices(True,True)
+grafo.vertices(ordered=True)
 
 #senza log
-grafo.vertices()
+#grafo.vertices()
 
 
 #edge_count
-grafo.edge_count()
+grafo.edge_count(True)
+
+#edges
+
+grafo.edges()
+
+#get_edge(self, u, v, log=False)
+grafo.get_edge(8, 37, True)
